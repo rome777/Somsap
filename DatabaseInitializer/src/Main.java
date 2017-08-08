784fd05dfc4bbf4532514582bd4c4fc029c58f72
Hoseop Lee <rome777@gmail.com>
2017-08-08 18:03:10 +0900

    DB Initializer 일단 만듦

---
+++ DatabaseInitializer/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+	public static void main(String[] args) {
+		ManageDatabase md = ManageDatabase.getInstance();
+		System.out.println("DROP TRIGGER Records_Seq_trigger: " +
+				md.excuteStatement("DROP TRIGGER RECORDS_SEQ_TRIGGER"));
+		System.out.println("DROP SEQUENCE Records_Seq: " +
+				md.excuteStatement("DROP SEQUENCE RECORDS_SEQ"));
+		System.out.println("DROP TABLE Records: " + md.deleteTable("Records"));
+		System.out.println("DROP TABLE Sessions: " + md.deleteTable("Sessions"));
+		System.out.println("DROP TABLE Users: " + md.deleteTable("Users"));
+		System.out.println("DROP TABLE User_Types: " + md.deleteTable("User_Types"));
+		System.out.println("DROP TABLE Comments: " + md.deleteTable("Comments"));
+		System.out.println("DROP TABLE Board_Posts: " + md.deleteTable("Board_Posts"));
+		System.out.println("DROP TABLE Board_Codes: " + md.deleteTable("Board_Codes"));
+		System.out.println();
+		System.out.println("CREATE TABLE UserTypes: " + md.createUserTypeTable());
+		System.out.println("CREATE TABLE Users: " + md.createUsersTable());
+		System.out.println("CREATE TABLE Sessions: " + md.createSessionsTable());
+		System.out.println("CREATE TABLE Records: " + md.createRecordsTable());
+		System.out.println("CREATE SEQUENCE Records_Seq: " + md.excuteStatement(
+			"CREATE SEQUENCE Records_Seq " +
+					"START WITH 1 " +
+					"INCREMENT BY 1 "
+		));
+		System.out.println("CREATE SEQUENCE Records_Seq: " + md.excuteStatement(
+			"CREATE OR REPLACE TRIGGER Records_Seq_trigger " +
+					"BEFORE INSERT ON Records REFERENCING NEW AS NEW " +
+					"FOR EACH ROW " +
+					"BEGIN " +
+						"SELECT Records_Seq.nextval INTO :NEW.Seq FROM dual; " +
+					"END"
+		));
+		System.out.println("CREATE TABLE BoardCodes: " + md.createBoardCodesTable());
+		System.out.println("CREATE TABLE BoardPosts: " + md.createBoardPostsTable());
+		System.out.println("CREATE TABLE Comments: " + md.createCommentsTable());
+	}
+}
---
+++ DatabaseInitializer/src/ManageDatabase.java
@@ -0,0 +1,286 @@
+import java.sql.*;
+
+public class ManageDatabase {
+	
+	private static ManageDatabase md;
+	
+	public static ManageDatabase getInstance() {
+		//static 변수가 null 일 때는 객체를 생성
+		if (md == null) {
+			md = new ManageDatabase();
+		}
+		//static 변수 리턴
+		return md;
+	}
+	
+	//여러 메소드에서 공통으로 사용할 변수
+	//데이터베이스 연결을 저장할 변수
+	private Connection con;
+	//Sql 실행을 위한 객체를 저장하기 위한 변수
+	private PreparedStatement pstmt;
+	private Statement stmt;
+	//select 구문의 결과를 저장하기 위한 변수
+	private ResultSet rs;
+	
+	//기본
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+	}
+	
+	private void connect() {
+		try {
+			String dbUrl = "jdbc:oracle:thin:@211.183.2.253:1521:orcl";
+			con = DriverManager.getConnection(dbUrl, "user01", "user01");
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		}
+	}
+	
+	private void close() {
+		try {
+			if (rs != null) rs.close();
+			if (stmt != null) stmt.close();
+			if (pstmt != null) pstmt.close();
+			if (con != null) con.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.getStackTrace();
+		}
+	}
+	
+	public boolean excuteStatement(String query){
+		boolean returnValue = false;
+		try {
+			connect();
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+			return returnValue;
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	public boolean deleteTable(String tableName){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+				/*"DECLARE " +
+				"I_CNT NUMBER; " +
+				"BEGIN " +
+					"SELECT COUNT(*) INTO I_CNT FROM ALL_TABLES WHERE TABLE_NAME = ?; " +
+					"IF I_CNT > 0 THEN EXECUTE IMMEDIATE ?; END IF; " +
+					//"DBMS_OUTPUT.PUT_LINE('삭제'); " +
+				"END;"*/
+				"DROP TABLE " + tableName.toUpperCase();
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+			//TODO 지워졌는지 안지워졌는지 확인하는 방법 찾다가 보류함
+			return returnValue;
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//UserType
+	public boolean createUserTypeTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE User_Types(" +
+						"Type VARCHAR2(16) PRIMARY KEY," +
+						"Name VARCHAR2(16) NOT NULL," +
+						"Is_Using NUMBER(1) DEFAULT 1 NOT NULL," +
+						"Created_Datetime TIMESTAMP NOT NULL," +
+						"Created_User_Email VARCHAR2(128) NOT NULL," +
+						"Modified_Datetime TIMESTAMP NOT NULL," +
+						"Modified_User_Email VARCHAR2(128) NOT NULL" +
+					")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//Users
+	public boolean createUsersTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+				"CREATE TABLE Users(" +
+					"Email VARCHAR2(128) PRIMARY KEY," +
+					"Type VARCHAR2(16)," +
+					"Nick_Name VARCHAR2(16) NOT NULL," +
+					"Password VARCHAR2(16) NOT NULL," +
+					"Is_Using NUMBER(1) DEFAULT 1 NOT NULL," +
+					"Created_Datetime TIMESTAMP NOT NULL," +
+					"Created_User_Email VARCHAR2(128) NOT NULL," +
+					"Modified_Datetime TIMESTAMP NOT NULL," +
+					"Modified_User_Email VARCHAR2(128) NOT NULL," +
+					"CONSTRAINT FK_Users_UserTypes FOREIGN KEY(Type) " +
+					"REFERENCES User_Types(Type)" +
+				")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//Sessions
+	public boolean createSessionsTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE Sessions(" +
+						"Email VARCHAR2(128) PRIMARY KEY," +
+						"Session_Id VARCHAR2(128)," +
+						"CONSTRAINT FK_Session_Users FOREIGN KEY(Email)" +
+						"REFERENCES Users(Email)" +
+					")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//Records
+	public boolean createRecordsTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE Records(" +
+						"Seq NUMBER PRIMARY KEY," +
+						"Created_Datetime TIMESTAMP," +
+						"Created_User_Email VARCHAR2(128)," +
+						"Location VARCHAR2(128)," +
+						"Before_Data VARCHAR2(1024)," +
+						"After_Data VARCHAR2(1024)," +
+						"CONSTRAINT FK_Records_Users FOREIGN KEY (Created_User_Email)" +
+						"REFERENCES Users(Email)" +
+					")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//BoardCodes
+	public boolean createBoardCodesTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE Board_Codes(" +
+							"Code VARCHAR2(16) PRIMARY KEY," +
+							"Name VARCHAR2(16) NOT NULL," +
+							"Is_Showing NUMBER(1) DEFAULT 1 NOT NULL" +
+							")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//BoardPosts
+	public boolean createBoardPostsTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE Board_Posts(" +
+							"Board_Code VARCHAR2(16)," +
+							"Seq NUMBER," +
+							"Parent_Board_Seq NUMBER," +
+							"Type VARCHAR2(16)," +
+							"Title VARCHAR2(256)," +
+							"Content CLOB," +
+							"Attachment_Loction VARCHAR2(256)," +
+							"Hits NUMBER DEFAULT 0 NOT NULL," +
+							"Likes NUMBER DEFAULT 0 NOT NULL," +
+							"Hates NUMBER DEFAULT 0 NOT NULL," +
+							"Is_Showing NUMBER(1) DEFAULT 1 NOT NULL," +
+							"CONSTRAINT PK_BoardPosts PRIMARY KEY (Board_Code, Seq)," +
+							"CONSTRAINT FK_BoardPosts_BoardCodes FOREIGN KEY (Board_Code)" +
+							"REFERENCES Board_Codes(Code)" +
+							")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+	
+	//Comments
+	public boolean createCommentsTable(){
+		boolean returnValue = false;
+		try {
+			connect();
+			String query =
+					"CREATE TABLE Comments(" +
+							"Board_Code VARCHAR2(16)," +
+							"Post_Seq NUMBER," +
+							"Seq NUMBER," +
+							"Parent_Comment_Seq NUMBER," +
+							"Content VARCHAR2(1024) NOT NULL," +
+							"Likes NUMBER DEFAULT 0 NOT NULL," +
+							"Hates NUMBER DEFAULT 0 NOT NULL," +
+							"Is_Showing NUMBER DEFAULT 1 NOT NULL," +
+							"CONSTRAINT PK_Comments PRIMARY KEY (Board_Code, Post_Seq, Seq)" +
+							")";
+			stmt = con.createStatement();
+			returnValue = stmt.executeUpdate(query) == 0;   //Create Drop은 0이면 성공, 실패하면 예외
+		} catch (SQLException e) {
+			System.out.print(e.getMessage());
+			e.getStackTrace();
+		} finally {
+			close();
+		}
+		return returnValue;
+	}
+}
